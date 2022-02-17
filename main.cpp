c470c1e7e643be62a9f494f6fa5d91af6fe78f25
Bilal Ehsan <bilalehsan933@gmail.com>
2022-02-17 22:16:42 +0000

    Refactored by introducing some auxiliary functions

--- main.cpp
+++ main.cpp
@@ -6,6 +6,47 @@
 #include <list>
 #include <unordered_map>
 
+void loadData(std::string, std::unordered_map<std::string, std::string>&);
+void tokensieInput(std::string&, char, std::unordered_map<std::string, std::string>&);
+
+int main(int argc, char* argv[]) {
+  std::unordered_map<std::string, std::string> bricks;
+  std::list<std::string> outputSequence;
+
+  loadData(argv[1], bricks);
+
+  const auto startingBrick = bricks.begin();
+
+  outputSequence.emplace_back(startingBrick->first);
+  outputSequence.emplace_back(startingBrick->second);
+
+  // Append to back of sequence
+  
+
+  return 0;
+}
+
+void loadData(std::string path, std::unordered_map<std::string, std::string>& bricks) {
+  std::ifstream ifs;
+  ifs.open(path);
+
+  if (!ifs) {
+    std::cerr << "Could not open file!" << std::endl;
+    return;
+  }
+
+  std::string line = "";
+
+  while (std::getline(ifs, line)) {
+    if (line == "") continue;
+
+    const char delim = ',';
+    tokensieInput(line, delim, bricks);
+  }
+
+  ifs.close();
+}
+
 void tokensieInput(std::string& str, char delim, std::unordered_map<std::string, std::string>& bricks) {
   std::stringstream ss(str);
   std::vector<std::string> brick = {};
@@ -17,27 +58,4 @@ void tokensieInput(std::string& str, char delim, std::unordered_map<std::string,
   std::string northSide = brick[0];
   std::string southSide = brick[1];
   bricks.emplace(northSide, southSide);
-}
-
-int main(int argc, char* argv[]) {
-  std::ifstream ifs;
-  ifs.open(argv[1]);
-
-  if (!ifs) {
-    std::cerr << "Could not open file!" << std::endl;
-    return -1;
-  }
-
-  std::string line = "";
-  std::unordered_map<std::string, std::string> bricks;
-
-  while (std::getline(ifs, line)) {
-    if (line == "") continue;
-
-    const char delim = ',';
-    tokensieInput(line, delim, bricks);
-  }
-
-  ifs.close();
-  return 0;
 }
