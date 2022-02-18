3230089306d697da7ef6e50582d83ebbdfa5ea01
Bilal Ehsan <bilalehsan933@gmail.com>
2022-02-18 22:07:46 +0000

    I'm slowly giving up, my motivation is low, and I don't know what to do. Send help :/

--- main.cpp
+++ main.cpp
@@ -5,15 +5,19 @@
 #include <string>
 #include <list>
 #include <unordered_map>
+#include <map>
 
 using northSide = std::string;
 using southSide = std::string;
 
 void loadData(std::string, std::unordered_map<northSide, southSide>&);
-void tokensieInput(std::string&, char, std::unordered_map<northSide, southSide>&);
+void tokeniseInput(std::string&, char, std::unordered_map<northSide, southSide>&);
+// void loadData(std::string, std::map<northSide, southSide>&);
+// void tokeniseInput(std::string&, char, std::map<northSide, southSide>&);
 
 int main(int argc, char* argv[]) {
   std::unordered_map<northSide, southSide> bricks;
+  // std::map<northSide, southSide> bricks;
   std::list<std::string> outputSequence;
 
   loadData(argv[1], bricks);
@@ -24,16 +28,22 @@ int main(int argc, char* argv[]) {
   outputSequence.emplace_back(startingBrick->second);
 
   for (const auto& brick : bricks) {
-    if (bricks.find(outputSequence.back()) == bricks.end()) {
-      auto front = bricks.find(outputSequence.front());
+    auto back = bricks.find(outputSequence.back());
 
-      for (const auto& brick : bricks) {
-        if (brick.second == front->first)
-          outputSequence.emplace_front(brick.first);
-      }
+    if (back == bricks.end()) {
+      break;
     } else {
-      auto back = bricks.find(outputSequence.back());
       outputSequence.emplace_back(back->second);
+    }
+  }
+
+  for (const auto& brick : bricks) {
+    auto front = bricks.find(outputSequence.front());
+    
+    for (const auto& brick : bricks) {
+      if (brick.second == front->first) {
+        outputSequence.emplace_front(brick.first);
+      } 
     }
   }
 
@@ -44,6 +54,7 @@ int main(int argc, char* argv[]) {
 }
 
 void loadData(std::string path, std::unordered_map<northSide, southSide>& bricks) {
+// void loadData(std::string path, std::map<northSide, southSide>& bricks) {
   std::ifstream ifs;
   ifs.open(path);
 
@@ -58,13 +69,14 @@ void loadData(std::string path, std::unordered_map<northSide, southSide>& bricks
     if (line == "") continue;
 
     const char delim = ',';
-    tokensieInput(line, delim, bricks);
+    tokeniseInput(line, delim, bricks);
   }
 
   ifs.close();
 }
 
-void tokensieInput(std::string& str, char delim, std::unordered_map<northSide, southSide>& bricks) {
+void tokeniseInput(std::string& str, char delim, std::unordered_map<northSide, southSide>& bricks) {
+// void tokeniseInput(std::string& str, char delim, std::map<northSide, southSide>& bricks) {
   std::stringstream ss(str);
   std::vector<std::string> brick = {};
 
