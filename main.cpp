c0f88e27cd1f9c11ead863e18f97a51e338e1bf8
Bilal-Ehsan <bilalehsan933@gmail.com>
2022-02-20 13:33:49 +0000

    Changed 'combination' to 'implementation' because I felt like it

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include "solution-1.h"
 #include "solution-2.h"
 
-void firstCombination(std::string path) {
+void firstImplementation(std::string path) {
   std::unordered_map<northSide, southSide> bricks = solution_1::loadData(path);
   std::list<std::string> outputSequence = solution_1::createSequence(bricks);
   solution_1::displaySequence(outputSequence);
 }
 
-void secondCombination(std::string path) {
+void secondImplementation(std::string path) {
   std::map<northSide, southSide> bricks = solution_2::loadData(path);
   std::list<std::string> outputSequence = solution_2::createSequence(bricks);
   solution_2::displaySequence(outputSequence);
 }
 
 int main(int argc, char* argv[]) {
-  firstCombination(argv[1]);
-  // secondCombination(argv[1]);
+  firstImplementation(argv[1]);
+  // secondImplementation(argv[1]);
 
   return 0;
 }
