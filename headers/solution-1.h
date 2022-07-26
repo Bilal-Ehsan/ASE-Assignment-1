cc8ebbbad7d34eaaf440088aca9ec269cc7820fd
Bilal-Ehsan <bilalehsan933@gmail.com>
2022-07-26 18:41:01 +0100

    Added parameter names to forward declarations instead of omitting them for better readability

--- headers/solution-1.h
+++ headers/solution-1.h
@@ -9,11 +9,11 @@ using northSide = std::string;
 using southSide = std::string;
 
 namespace solution_1 {
-  std::unordered_map<northSide, southSide> loadData(const std::string);
-  void tokeniseInput(std::string&, char, std::unordered_map<northSide, southSide>&);
-  std::list<std::string> createSequence(std::unordered_map<northSide, southSide>&);
-  std::unordered_map<southSide, northSide> inverseMap(std::unordered_map<northSide, southSide>&);
-  void displaySequence(std::list<std::string>&);
+  std::unordered_map<northSide, southSide> loadData(const std::string path);
+  void tokeniseInput(std::string& str, char delim, std::unordered_map<northSide, southSide>& bricks);
+  std::list<std::string> createSequence(std::unordered_map<northSide, southSide>& bricks);
+  std::unordered_map<southSide, northSide> inverseMap(std::unordered_map<northSide, southSide>& map);
+  void displaySequence(std::list<std::string>& seq);
 }
 
 #endif
--- headers/solution-2.h
+++ headers/solution-2.h
@@ -9,11 +9,11 @@ using northSide = std::string;
 using southSide = std::string;
 
 namespace solution_2 {
-  std::map<northSide, southSide> loadData(const std::string);
-  void tokeniseInput(std::string&, char, std::map<northSide, southSide>&);
-  std::list<std::string> createSequence(std::map<northSide, southSide>&);
-  std::map<southSide, northSide> inverseMap(std::map<northSide, southSide>&);
-  void displaySequence(std::list<std::string>&);
+  std::map<northSide, southSide> loadData(const std::string path);
+  void tokeniseInput(std::string& str, char delim, std::map<northSide, southSide>& bricks);
+  std::list<std::string> createSequence(std::map<northSide, southSide>& bricks);
+  std::map<southSide, northSide> inverseMap(std::map<northSide, southSide>& map);
+  void displaySequence(std::list<std::string>& seq);
 }
 
 #endif
